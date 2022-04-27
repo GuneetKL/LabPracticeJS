56ca77f736e8eff0417853a9f7c62d5048411f98
Guneet_Kaur <98298794+GuneetKL@users.noreply.github.com>
2022-04-27 01:30:19 -0700

    Update Lab10_part2.js

--- script/Lab10_part2.js
+++ script/Lab10_part2.js
@@ -2,23 +2,45 @@
 
  var leftPosition = -200;
 
- var dest_x = screen.width;
- var x = 0;
- var interval = 5;
+ //var dest_x = screen.width;
+ //var x = 0;
+ //var interval = 5;
+
 
  function runPuppy() {
 
 
      var puppy = document.getElementById("puppy");
 
-     if (x < dest_x) {
-         puppy.style.left = x + "px";
-         x = x + interval;
+
+     puppy.style.left = leftPosition + "px";
+     puppy.style.top = topPosition + "px";
+     leftPosition += 50;
+     topPosition += 10;
+
+     //  x = x + interval;
+     if (leftPosition >= screen.availWidth) {
+         leftPosition = -200;
 
      }
+     if (topPosition >= 600) {
+         topPosition = 200;
+     }
+  
+  
+ //function runPuppy() {
+
+
+ //    var puppy = document.getElementById("puppy");
+
+ //    if (x < dest_x) {
+ //        puppy.style.left = x + "px";
+ //        x = x + interval;
+
+//     }
 
 
 
      // add
      // if it reaches the screen end(you can check with screen.availwidth)
- }
+ //}
